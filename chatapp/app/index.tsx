ecdceb2258779c993dee8de0429f174b882a3292
Monish R <radhakrishnanmonish@gmail.com>
2026-02-11 23:06:03 +0530

    Initial commit

---
+++ chatapp/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text,Image } from "react-native";
+import "../global.css"
+
+export default function WelcomeScreen() {
+    return <>
+     < Image className="width -28 height-28 mb-10 "  source ={require("../assets/images/vbn.png")}/>
+    </>
+
+}
