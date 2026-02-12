e852e61548717d81003e8083028911186a07930b
Monish R <radhakrishnanmonish@gmail.com>
2026-02-12 15:38:32 +0530

    add gitignore and remove node_modules

--- chatapp/app/index.tsx
+++ chatapp/app/index.tsx
@@ -1,9 +1,21 @@
-import { Text,Image } from "react-native";
-import "../global.css"
+// app/index.tsx
+import React from "react";
+import { Text,Image, View, TouchableOpacity } from "react-native";
+import "../global.css";
+import { useRouter } from "expo-router";
 
 export default function WelcomeScreen() {
-    return <>
-     < Image className="width -28 height-28 mb-10 "  source ={require("../assets/images/vbn.png")}/>
-    </>
-
+  const router=useRouter();
+  return (
+    <View className="flex-1 items-center justify-center px-5">
+    <Image className="w-50 h-45" source ={require("../assets/images/print-shop.png")}/>
+    <Text className="text-3xl font-bold text-gray-900 text-center mb-2">Welcome</Text>
+    <Text className="text-center text-lg mb-8">Read Our
+         <Text className="text-blue-500"> Privacy Policy</Text> .Tap"Agree & Continue"</Text>
+          <Text className="text-blue-500 mb-8">Terms and Conditions</Text>
+    <TouchableOpacity className="bg-green-500 px-6 py-4 rounded-full w-full" onPress={()=>router.push("/login")}>
+      <Text className="text-white text-center text bold text-lg">Agree and Continue</Text>
+    </TouchableOpacity>
+    </View>
+  );
 }
---
+++ chatapp/app/login.tsx
@@ -0,0 +1,44 @@
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Alert,View, Text, TextInput, TouchableOpacity} from "react-native"
+export default function LoginScreen(){
+    const[phone, setPhone]=useState("+91");
+    const router=useRouter();
+    //whatsapp phone number validation
+    const isValidNumber = /^\+\d{2}\s?\d{10}$/.test(phone);
+
+    const handleNext = () => {
+        if(isValidNumber){
+            Alert.alert("Invalid phone number.");
+            return;
+        }
+        router.push({pathname:"/otp",params:{phone}})
+    }
+
+    return(
+        <View className="flex-1 items-center justify-center px-5"> 
+            {/*Heading*/}
+            <Text className="text-3xl font-bold text-gray-900 mb-4">Enter Your Phone Number</Text>
+
+            {/*Description*/}
+            <Text className="text-gray-500 text-lg text-center mb-6">WhatsApp will send an SMS to verify your Number</Text>
+            
+            {/*Phone Number Input*/}
+            <TextInput
+            className="border border-gray-300 rounded-lg px-4  w-full text-center"
+                placeholder= "+91 1234567890"
+                keyboardType= "phone-pad"
+                value={phone}
+                onChangeText={setPhone}
+            />
+            {/* Next Button*/}
+        <TouchableOpacity
+        className={`p-4 w-full ${isValidNumber ? "bg-green-500" : "bg-gray-300"}`}
+        disabled={!isValidNumber}
+        onPress={handleNext}
+        >
+        <Text className="text-white text-center font-bold text-lg">Next</Text>
+        </TouchableOpacity>
+        </View>
+    );
+}
